d3cf0fbecb86be3ad1be07d390452416762f54a0
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-01 09:42:47 -0500

    init

---
+++ src/main/java/com/isai/demo_system_management_trailers/DemoSystemManagementTrailersApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_system_management_trailers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSystemManagementTrailersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSystemManagementTrailersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_system_management_trailers/DemoSystemManagementTrailersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_system_management_trailers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSystemManagementTrailersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
